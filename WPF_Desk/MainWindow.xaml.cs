cc4088fee2e287f17df9ee3bb56df0a0578208cb
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-04-28 02:48:53 +0300

    initiall commit

--- WPF_Desk/MainWindow.xaml.cs
+++ WPF_Desk/MainWindow.xaml.cs
@@ -1,155 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Windows;
-using System.Windows.Input;
 
-namespace NumberGenerator
+namespace RandomNumberGenerator
 {
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = this;
-            GenerateNumbersCommand = new RelayCommand(GenerateNumbers_Click);
         }
 
-        public ICommand GenerateNumbersCommand { get; }
-
-        private string _statistics;
-        public string Statistics
+        private void OnGenerateAndSaveClick(object sender, RoutedEventArgs e)
         {
-            get { return _statistics; }
-            set
+            try
             {
-                _statistics = value;
-                OnPropertyChanged(nameof(Statistics));
-            }
-        }
+                Random random = new Random();
+                int[] numbers = new int[10000];
 
-        private string _result;
-        public string Result
-        {
-            get { return _result; }
-            set
-            {
-                _result = value;
-                OnPropertyChanged(nameof(Result));
-            }
-        }
-
-        private void GenerateNumbers_Click()
-        {
-            // Генеруємо 100 цілих чисел
-            List<int> numbers = GenerateNumbers(100);
-
-            // Зберігаємо прості числа у файл
-            List<int> primeNumbers = GetPrimeNumbers(numbers);
-            SaveNumbersToFile("prime_numbers.txt", primeNumbers);
-
-            // Зберігаємо числа Фібоначчі у файл
-            List<int> fibonacciNumbers = GetFibonacciNumbers(numbers);
-            SaveNumbersToFile("fibonacci_numbers.txt", fibonacciNumbers);
-
-            // Формуємо статистику
-            Statistics = $"Згенеровано чисел: {numbers.Count}\n" +
-                         $"Кількість простих чисел: {primeNumbers.Count}\n" +
-                         $"Кількість чисел Фібоначчі: {fibonacciNumbers.Count}";
-
-            // Формуємо рядок результату
-            Result = string.Join(Environment.NewLine, numbers.Select(n => n.ToString()));
-        }
-
-        private List<int> GenerateNumbers(int count)
-        {
-            Random random = new Random();
-            List<int> numbers = new List<int>();
-            for (int i = 0; i < count; i++)
-            {
-                numbers.Add(random.Next(1, 1000)); // Згенеруємо числа від 1 до 1000
-            }
-            return numbers;
-        }
-
-        private List<int> GetPrimeNumbers(List<int> numbers)
-        {
-            List<int> primes = new List<int>();
-            foreach (int number in numbers)
-            {
-                if (IsPrime(number))
+                for (int i = 0; i < numbers.Length; i++)
                 {
-                    primes.Add(number);
+                    numbers[i] = random.Next(1, 10001); 
                 }
-            }
-            return primes;
-        }
 
-        private bool IsPrime(int number)
-        {
-            if (number <= 1) return false;
-            if (number == 2) return true;
-            if (number % 2 == 0) return false;
-            int sqrt = (int)Math.Sqrt(number);
-            for (int i = 3; i <= sqrt; i += 2)
+                var evenNumbers = numbers.Where(n => n % 2 == 0).Select(n => n.ToString()).ToArray();
+                var oddNumbers = numbers.Where(n => n % 2 != 0).Select(n => n.ToString()).ToArray();
+
+                // Запис у файли
+                string evenFilePath = "even_numbers.txt";
+                string oddFilePath = "odd_numbers.txt";
+
+                File.WriteAllLines(evenFilePath, evenNumbers);
+                File.WriteAllLines(oddFilePath, oddNumbers);
+
+                EvenCountTextBlock.Text = $"Парних чисел: {evenNumbers.Length}";
+                OddCountTextBlock.Text = $"Непарних чисел: {oddNumbers.Length}";
+
+                MessageBox.Show("Генерація та збереження завершено.");
+            }
+            catch (Exception ex)
             {
-                if (number % i == 0)
-                {
-                    return false;
-                }
+                MessageBox.Show($"Помилка: {ex.Message}");
             }
-            return true;
         }
-
-        private List<int> GetFibonacciNumbers(List<int> numbers)
-        {
-            List<int> fibonacci = new List<int>();
-            foreach (int number in numbers)
-            {
-                if (IsFibonacci(number))
-                {
-                    fibonacci.Add(number);
-                }
-            }
-            return fibonacci;
-        }
-
-        private bool IsFibonacci(int number)
-        {
-            double sqrt5 = Math.Sqrt(5);
-            double phi = (1 + sqrt5) / 2;
-            int n = (int)Math.Floor(Math.Log(number * sqrt5) / Math.Log(phi) + 0.5);
-            int fib = (int)(Math.Pow(phi, n) / sqrt5 + 0.5);
-            return fib == number;
-        }
-
-        private void SaveNumbersToFile(string fileName, List<int> numbers)
-        {
-            File.WriteAllLines(fileName, numbers.Select(n => n.ToString()));
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected virtual void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
-
-    public class RelayCommand : ICommand
-    {
-        private readonly Action _execute;
-
-        public event EventHandler CanExecuteChanged;
-
-        public RelayCommand(Action execute)
-        {
-            _execute = execute;
-        }
-
-        public bool CanExecute(object parameter) => true;
-
-        public void Execute(object parameter) => _execute();
     }
 }
